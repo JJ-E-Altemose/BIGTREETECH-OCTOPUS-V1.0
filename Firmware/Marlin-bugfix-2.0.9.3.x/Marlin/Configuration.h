c1eb62d354b404bf8999662d4fa5c0a8e27a4b7a
JJ-E-Altemose <jjea@colostate.edu>
2024-08-20 21:35:19 -0600

    Update Configuration.h

--- Firmware/Marlin-bugfix-2.0.9.3.x/Marlin/Configuration.h
+++ Firmware/Marlin-bugfix-2.0.9.3.x/Marlin/Configuration.h
@@ -1019,7 +1019,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 207.42723318835732 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 207.42723318835732 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
